f66ce7f50bbe98027b22abf8dc68a61bcc649a2c
ashu3103 <ash2003sharma@gmail.com>
2022-01-29 23:50:35 +0530

    Added research lab page

--- src/components/Research.jsx
+++ src/components/Research.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { ResearchLab } from './researchLab';
 
 export const Research = (props) => {
-  return <div>
-      <h2>Research labs</h2>
+  return (<div>
+      <ResearchLab/>
       <h2>Research centers</h2>
       <h2>Project Fundings</h2>
       <h2>I HUB</h2>
       <h2>Catalyst</h2>
       <h2>Research News</h2>
-  </div>;
+  </div>);
 };
