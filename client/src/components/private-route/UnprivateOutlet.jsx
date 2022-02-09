c52175908e717c8f98d56bc918486ef760361efa
Rijul Jain <jrijul1201@gmail.com>
2022-02-09 18:27:00 +0530

    Added unprivate routing

--- client/src/components/private-route/UnprivateOutlet.jsx
+++ client/src/components/private-route/UnprivateOutlet.jsx
@@ -7,7 +7,11 @@ const PrivateOutlet = ({ component: Component, auth, ...rest }) =>
   !auth.isAuthenticated ? (
     <Outlet />
   ) : (
-    <Navigate to="/" />
+    <Navigate
+      to={
+        "/dashboard" + auth.user.role[0].toUpperCase() + auth.user.role.slice(1)
+      }
+    />
   );
 
 PrivateOutlet.propTypes = {
