9837eb72341aa005fe9c88c9c82e9ce8093a0868
dipesh809 <b20096@students.iitmandi.ac.in>
2022-02-15 23:21:54 +0530

    styled acadmic page

--- client/src/components/school.jsx
+++ client/src/components/school.jsx
@@ -17,49 +17,63 @@ export const Schools = (props) => {
               institute has 4 schools.
             </p>
             <br />
-            <h3 className="subHead">
-              School of Computing and Electrical Engineering
-            </h3>
-            <p>
-              This School brings together faculty involved in the key
-              technologies of the Information Age. These include computer
-              science, communication, VLSI and microelectronics, and electrical
-              energy. The underlying fundamental principles are information
-              theory, theory of computation, communication theory, quantum
-              mechanics and the laws of electromagnetism.
-            </p>
+            <div className="sec-heading">
+              <div className="subHead">
+                School of Computing and Electrical Engineering
+              </div>
+              <div className="sub-content">
+                This School brings together faculty involved in the key
+                technologies of the Information Age. These include computer
+                science, communication, VLSI and microelectronics, and electrical
+                energy. The underlying fundamental principles are information
+                theory, theory of computation, communication theory, quantum
+                mechanics and the laws of electromagnetism.
+              </div>
+            </div>
             <br />
-            <h3 className="subHead">School of Engineering</h3>
-            <p>
+            <div className="sec-heading">
+              <div className="subHead">
+                School of Engineering
+              </div>
+              <div className="sub-content">
               This School covers tangible physical structures and artifacts such
               as transport vehicles, transport systems, machines, materials,
               manufacturing, designs etc. The underlying principles are
               classical mechanics, atomic physics, thermodynamics. Many faculty
               from the traditional departments of mechanical, civil, aerospace,
               metallurgy engineering would be in this School.
-            </p>
+              </div>
+            </div>
             <br />
-            <h3 className="subHead">School of Basic Sciences</h3>
-            <p>
+            <div className="sec-heading">
+              <div className="subHead">
+              School of Basic Sciences
+              </div>
+              <div className="sub-content">
               This School includes mathematics, physics, chemistry and life
               sciences. While some faculty may work in pure research, others
               would work on applied research in collaboration with colleagues in
               the engineering Schools
-            </p>
+              </div>
+            </div>
             <br />
-            <h3 className="subHead">
+            <div className="sec-heading">
+              <div className="subHead">
               School of Humanities and Social Sciences
-            </h3>
-            <p>
+              </div>
+              <div className="sub-content">
               Modern engineers work in teams to create, improve and apply
               technology for the good of society. A good understanding of
               language, culture, sociology, economics, management, ecology, etc.
               is essential for the well-rounded engineer, for development of
               technologies, products and processes that will see widespread use.
               This School is thus an important part of IIT Mandi.
-            </p>
+              </div>
+            </div>
+            <br />
           </div>
         </div>
+        <br /><br /><br />
       </div>
     </>
   );
