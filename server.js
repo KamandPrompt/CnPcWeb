1025b8af24846c5aea363112f14e3e442e346363
Rijul Jain <jrijul1201@gmail.com>
2022-02-15 14:28:03 +0530

    removed config.env

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config.env" });
+// require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
