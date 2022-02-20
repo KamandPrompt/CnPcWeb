941b6e63ab9a68f99a0a4851c9ab28465f81a2ab
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2022-02-20 19:18:53 +0530

    Displaying students data properly

--- client/src/components/layout/AdminDashboard.jsx
+++ client/src/components/layout/AdminDashboard.jsx
@@ -18,7 +18,7 @@ class AdminDashboard extends Component {
   async componentDidMount() {
     await axios.get("/api/admins/all-students")
     .then(res => {
-      this.setState({studentsData : res.data});
+      this.setState({studentsData : res.data, DataisLoaded:true});
       // console.log(this.state.studentsData);
     })
     .catch(err=> {
@@ -31,203 +31,90 @@ class AdminDashboard extends Component {
   };
   render() {
     const { user } = this.props.auth;
-    // const {userRows} = this.state.userRows;
-    const userRows = [
-      {
-        id: 1,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 2,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$130.00",
-      },
-      {
-        id: 3,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 4,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 5,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 6,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 7,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 8,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 9,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-      {
-        id: 10,
-        username: "Jon Snow",
-        avatar:
-          "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        email: "jon@gmail.com",
-        status: "active",
-        transaction: "$120.00",
-      },
-    ];
-    
-    const dum_columns = [
-      { field: "id", headerName: "ID", width: 90 },
-      {
-        field: "user",
-        headerName: "User",
-        width: 200,
-        renderCell: (params) => {
-          return (
-            <div className="userListUser">
-              <img className="userListImg" src={params.row.avatar} alt="" />
-              {params.row.username}
+    if(this.state.DataisLoaded === true)
+    {
+      const userRows = this.state.studentsData;
+      const columns = [
+        { field: 'rollNo', headerName: 'Roll No.'},
+        { field: 'name', headerName: 'Name'},
+        {
+          field: 'batch',
+          headerName: 'Batch',
+          type: 'number',
+        },
+        {
+          field: "verification_status",
+          headerName: "Status",
+        },
+        {
+          field: "edit",
+          headerName: "Edit",
+          renderCell: (params) => {
+            return (
+              <>
+                <Link to={"user/edit/" + params.row.rollNo}>
+                  <button className="userEdit">Edit</button>
+                </Link>
+              </>
+            );
+          },
+        },
+        {
+          field: "view",
+          headerName: "View",
+          renderCell: (params) => {
+            return (
+              <>
+                <Link to={"user/view/" + params.row.rollNo}>
+                  <button className="userView">View</button>
+                </Link>
+              </>
+            );
+          },
+        },
+      ];
+      return (
+        <>
+        <div  className="container text-center mt-15">
+          <div className="row">
+            <div className="col-sm-12">
+              <h4>
+                Hey there, <b className="name-lable">{user.name.split(" ")[0]}</b>
+                <p className="mt-4">
+                  You are logged into a full-stack{" "}
+                  <span style={{ fontFamily: "monospace" }}>admin</span> app 👏
+                </p>
+              </h4>
+              <div style={{height:'300px', width:'100%', fontSize:'1em'}}>
+                <div style={{ display: 'flex', height: '100%'}}>
+                <DataGrid
+                  rows={userRows}
+                  columns={[...columns,{ field: 'edit', sortable: false,filterable:false,hideable:false },{ field: 'view', sortable: false,filterable:false,hideable:false }]}
+                  pageSize={10}
+                  rowsPerPageOptions={[10,20,30]}
+                  style = {{fontSize:'15px'}}
+                  getRowId={(row) => row.rollNo}
+                />
+                </div>
+              </div>
+              {/* <button
+                onClick={this.onLogout}
+                className="btn btn-large btn-light hoverable font-weight-bold"
+              >
+                Logout
+              </button> */}
             </div>
-          );
-        },
-      },
-      { field: "email", headerName: "Email", width: 200 },
-      {
-        field: "status",
-        headerName: "Status",
-        width: 120,
-      },
-      {
-        field: "transaction",
-        headerName: "Transaction Volume",
-        width: 160,
-      },
-      {
-        field: "action",
-        headerName: "Action",
-        width: 150,
-        renderCell: (params) => {
-          return (
-            <>
-              <Link to={"/user/" + params.row.id}>
-                <button className="userListEdit">Edit</button>
-              </Link>
-            </>
-          );
-        },
-      },
-    ];
-    const columns = [
-      { field: 'rollNum', headerName: 'Roll No.', width: 70 },
-      { field: 'name', headerName: 'Name', width: 130 },
-      {
-        field: 'batch',
-        headerName: 'Batch',
-        type: 'number',
-        width: 90,
-      },
-      {
-        field: "status",
-        headerName: "Status",
-        width: 120,
-      },
-      {
-        field: "action",
-        headerName: "Action",
-        width: 150,
-        renderCell: (params) => {
-          return (
-            <>
-              <Link to={"/user/" + params.row.id}>
-                <button className="userListEdit">Edit</button>
-              </Link>
-            </>
-          );
-        },
-      },
-    ];
-    console.log(userRows);
-    return (
-      <>
-      <div  className="container text-center mt-15">
-        <div className="row">
-          <div className="col-sm-12">
-            <h4>
-              Hey there, <b className="name-lable">{user.name.split(" ")[0]}</b>
-              <p className="mt-4">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>admin</span> app 👏
-              </p>
-            </h4>
-            <div style={{ height: 600, fontSize:'1em'}}>
-              <DataGrid
-                rows={userRows}
-                columns={dum_columns}
-                pageSize={10}
-                rowsPerPageOptions={[10,20,30]}
-                style = {{fontSize:'15px'}}
-              />
-            </div>
-            {/* <button
-              onClick={this.onLogout}
-              className="btn btn-large btn-light hoverable font-weight-bold"
-            >
-              Logout
-            </button> */}
           </div>
         </div>
-      </div>
-      </>
-    );
+        </>
+      );
+    }
+    else
+    {
+      return (
+        <div>Wait for some time</div>
+      );
+    }
   }
 }
 
