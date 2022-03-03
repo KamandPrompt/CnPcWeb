983f179fe40e012a31d9c35f35923dbe92088a00
ashu3103 <ash2003sharma@gmail.com>
2022-03-03 19:03:05 +0530

    Styled forms

--- client/src/components/layout/LoginRecruiter.js
+++ client/src/components/layout/LoginRecruiter.js
@@ -50,8 +50,56 @@ class Login extends Component{
         const {email, password, errors} = this.state;
         return(
           <>
-       
-            <div className="form-box">
+            <div className="formCont">
+              <div className="formleft">
+              <div className="decal">
+              <div><i class="fa fa-graduation-cap graduation_cap" aria-hidden="true"></i></div>
+              <div className="header2"><h3 style={{"fontSize":"20px","textAlign":"center"}}>Career and Placement Cell</h3></div>
+              </div>
+              </div>
+              <div className="formright">
+                <form className="form_container" onSubmit={this.onSubmit}>
+                <h2 style={{"textAlign":"center","color":"white","paddingTop":"40px","paddingBottom":"20px","fontSize":"25px"}}>Login</h2>
+                <div className="form-group">
+                    <label style={{"color":"white"}}>Email Address</label>
+                    <input type="email" 
+                           id="email"
+                           placeholder="Email Address" 
+                           value={email} 
+                           error={errors}
+                           onChange={this.onChange}
+                           className={classnames("form-control", {
+                            invalid: errors.email || errors.emailnotfound
+                          })}/>
+                          <span className="red-text">
+                            {errors.email}
+                            {errors.emailnotfound}
+                        </span>
+                </div>
+                <div className="form-group">
+                    <label style={{"color":"white"}}>Password</label>
+                    <input type="password"  
+                           id="password" 
+                           placeholder="Password" 
+                           value={password} 
+                           error={errors}
+                           onChange={this.onChange}
+                           className={classnames("form-control", {
+                            invalid: errors.password || errors.passwordincorrect })}
+                            />
+                    <span className="red-text">
+                        {errors.password}
+                        {errors.passwordincorrect}
+                    </span>
+                </div>
+                <div className="text-center no_account">Don't have an account? <Link to="/registerStudent">Register</Link></div>
+                <div className="form-group form_sub">
+                    <button type="submit" className="btn btn-primary btn-block btn-lg" style={{"backgroundColor":"#CFFFF6","color":"black"}}>Login</button>
+                </div>
+                </form>
+              </div>
+            </div>
+            {/* <div className="form-box">
             <form className="form-container" onSubmit={this.onSubmit}>
                 <h2>Login</h2>
                 <hr/>
@@ -92,7 +140,7 @@ class Login extends Component{
 
 
             </form>
-        </div>
+        </div> */}
       </>
 
         )
