cb70187a54e25e4ad978e8d2c4fea3eee8f524c1
Harshit Krishna <b20103@students.iitmandi.ac.in>
2022-04-10 12:01:30 +0530

    kamand 70%

--- client/src/components/Kamand.jsx
+++ client/src/components/Kamand.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import  village  from "../images/village.jpeg"
-
+import village from "../images/village.jpeg";
+import manali from "../images/manali.jpg"
+import parashar from "../images/parashar.jpg"
 import "./kamand.css";
 export const Kamand = () => {
   return (
@@ -25,28 +26,94 @@ export const Kamand = () => {
       </div>
       <br />
       <div>
-          <hr className="left" />
-          <h3 className="homeHead">Guest House</h3>
-          <hr className="right" />
-          </div>
+        <hr className="left" />
+        <h3 className="homeHead">Guest House</h3>
+        <hr className="right" />
+      </div>
       <br />
       <div id="front" className="container-item">
         <div className="guestHouse1">
           {/* <h2 className="faculty-head">About Us</h2> */}
-          <p className="guest" style={{"textAlign":"justify"}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo vero commodi corrupti at? Mollitia, natus dicta! Quo quidem eligendi, corporis consectetur eos numquam id a, voluptate vitae, amet debitis sequi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepe, ullam suscipit aperiam, reiciendis aut voluptatum fuga voluptas repellendus enim perspiciatis quaerat a amet facere molestias quibusdam? Eum, saepe? Non amet necessitatibus, dolor ea placeat in assumenda maiores ut ducimus nemo beatae aut labore ipsa alias aliquid nostrum fugit, officiis maxime! Pariatur suscipit voluptas ipsa sit vero quae, illo est, ad animi corporis molestias magni expedita labore possimus necessitatibus sequi delectus dicta autem. Molestiae, ab tenetur perspiciatis sequi voluptates delectus soluta itaque iusto, quas mollitia iste voluptas reprehenderit debitis optio sint culpa maiores! Quisquam eos temporibus cupiditate? Officiis, esse maiores!
+          <p className="guest" style={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo vero
+            commodi corrupti at? Mollitia, natus dicta! Quo quidem eligendi,
+            corporis consectetur eos numquam id a, voluptate vitae, amet debitis
+            sequi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            saepe, ullam suscipit aperiam, reiciendis aut voluptatum fuga
+            voluptas repellendus enim perspiciatis quaerat a amet facere
+            molestias quibusdam? Eum, saepe? Non amet necessitatibus, dolor ea
+            placeat in assumenda maiores ut ducimus nemo beatae aut labore ipsa
+            alias aliquid nostrum fugit, officiis maxime! Pariatur suscipit
+            voluptas ipsa sit vero quae, illo est, ad animi corporis molestias
+            magni expedita labore possimus necessitatibus sequi delectus dicta
+            autem. Molestiae, ab tenetur perspiciatis sequi voluptates delectus
+            soluta itaque iusto, quas mollitia iste voluptas reprehenderit
+            debitis optio sint culpa maiores! Quisquam eos temporibus
+            cupiditate? Officiis, esse maiores!
           </p>
         </div>
         <div className="guestHouse2">
           <img
             className="village"
-              src={village}
-              alt="village"
-              height={270}
-              width={500}
-            />
+            src={village}
+            alt="village"
+            height={270}
+            width={500}
+          />
         </div>
+        {/*Carousel yaha add karna h */}
       </div>
+      <br />
+      <div>
+        <hr className="left" />
+        <h3 className="homeHead">Places to Visit</h3>
+        <hr className="right" />
+      </div>
+      <div id="front" className="container-item2">
+        <div className="places">
+          {/* <h2 className="faculty-head">About Us</h2> */}
+          <p className="guest" style={{ textAlign: "justify" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo vero
+            commodi corrupti at? Mollitia, natus dicta! Quo quidem eligendi,
+            corporis consectetur eos numquam id a, voluptate vitae, amet debitis
+            sequi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            saepe, ullam suscipit aperiam, reiciendis aut voluptatum fuga
+            voluptas repellendus enim perspiciatis quaerat a amet facere
+            molestias quibusdam? Eum, saepe? Non amet necessitatibus, dolor ea
+            placeat in assumenda maiores ut ducimus nemo beatae aut labore ipsa
+            alias aliquid nostrum fugit, officiis maxime! Pariatur suscipit
+            voluptas ipsa sit vero quae, illo est, ad animi corporis molestias
+            magni expedita labore possimus necessitatibus sequi delectus dicta
+            autem. Molestiae, ab tenetur perspiciatis sequi voluptates delectus
+            soluta itaque iusto, quas mollitia iste voluptas reprehenderit
+            debitis optio sint culpa maiores! Quisquam eos temporibus
+            cupiditate? Officiis, esse maiores!
+          </p>
+        </div>
+          {/* <div className="toVisit"> */}
+            <div className="ashu">
+            <div className="guestHouse2">
+              <img
+                className="village"
+                src={parashar}
+                alt="village"
+              />
+              <h3 style={{"color":"white"}}>Parashar Lake</h3>
+              <button style={{"color":"#07748B", "backgroundColor":"#58C1EE"}}>Know More</button>
+            </div>
+            <div className="guestHouse2">
+              <img
+                className="village"
+                src={manali}
+                alt="village"
+              />
+              <h3 style={{"color":"white"}}>Manali</h3>
+              <button style={{"color":"#07748B", "backgroundColor":"#58C1EE"}}>Know More</button>
+            </div>
+            </div>
+        {/* </div> */}
+      </div>
+      <br />
     </div>
   );
 };
