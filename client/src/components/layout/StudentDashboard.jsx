d2adbbbbef9b6ae167aa38aa962ac44c06c72c4f
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2022-04-25 16:11:02 +0530

    Small change

--- client/src/components/layout/StudentDashboard.jsx
+++ client/src/components/layout/StudentDashboard.jsx
@@ -197,7 +197,7 @@ class StudentDashboard extends Component {
                           <label>Gender</label>
                           </div>
                           <Form.Select className="btn-sm primary" aria-label="Default select example" onChange={this.onchange} id="Gender">
-                            <option value={user.Gender}>{user.Gender}</option>
+                            <option value={user.Gender} selected>{user.Gender}</option>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
                             <option value="Others">Others</option>
